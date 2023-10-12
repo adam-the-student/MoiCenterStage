456e420aa61ad53d3b695d55e2b0821822a4ef41
Fawwaz Shaphy <fawwaz.shaphy@gmail.com>
2023-10-12 17:58:27 -0500

    Fix TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/PTDTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/PTDTeleOp.java
@@ -16,15 +16,18 @@ public class PTDTeleOp extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x*1.15 ; // Counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
 
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double motor1Power = (y + x - rx) / denominator;  //motor1 is top left corner
-            double motor2Power = (y + x - rx) / denominator;  //motor2 is top right corner
-            double motor3Power = (y + x - rx) / denominator;  //motor3 is bottom left corner
-            double motor4Power = (y + x - rx) / denominator;  //motor4 is bottom right corner
+            double motor1Power = (y + x + rx) / denominator;
+            double motor2Power = (y - x + rx) / denominator;
+            double motor3Power = (y - x - rx) / denominator;
+            double motor4Power = (y + x - rx) / denominator;
 
             if(gamepad1.left_trigger >= 0.1){
                 motor1Power = motor1Power/4;
