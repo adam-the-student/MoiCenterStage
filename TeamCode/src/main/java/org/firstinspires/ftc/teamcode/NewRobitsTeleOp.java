43c99d2002b499151cb5bb5fe37dbf3c7a1a62bf
Fawwaz Shaphy <fawwaz.shaphy@gmail.com>
2024-02-08 17:29:24 -0600

    Robit tele - fawwaz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobitsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobitsTeleOp.java
@@ -6,51 +6,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImpl;
 
-@TeleOp(name = "Meet TeleOP")
+@TeleOp(name = "ROBITS TELE")
 public class NewRobitsTeleOp extends LinearOpMode {
     DcMotor motor1,motor2, armMotor;
-    Servo ignition, yellowPixel;
     @Override
     public void runOpMode(){
         motor1 = hardwareMap.get(DcMotor.class,"leftMotor");
         motor2 = hardwareMap.get(DcMotor.class, "rightMotor");
         armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        ignition = hardwareMap.get(Servo.class,"ign");
-        yellowPixel = hardwareMap.get(Servo.class,"yellowPixel");
 
-        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
-
-        yellowPixel.scaleRange(0.6,.7);
-        yellowPixel.setPosition(1);
-
-        boolean e = false;
+        boolean gm2 = false;
         while (opModeIsActive()){
+            gm2 = gamepad2.right_bumper != gm2;
 
-            double verticalPower = gamepad1.right_trigger>0?-gamepad1.right_trigger:gamepad1.left_trigger;
-            if (gamepad2.right_bumper){
-                verticalPower/=4;
-            }
-            double turnPower = -gamepad1.left_stick_x/3;
-            motor1.setPower((verticalPower+turnPower)/Math.max(1,verticalPower+turnPower));
-            motor2.setPower((turnPower-verticalPower)/Math.max(1,verticalPower+turnPower));
-
-            if(gamepad1.dpad_down){
-                e=true;
-            }
-
-            if (e){
-                armMotor.setPower(-1);
+            if (!gm2) {
+                motor1.setPower((gamepad1.left_stick_y + gamepad1.right_stick_x) / (gamepad2.right_trigger * 2 + 1) / 2 * (gamepad2.left_trigger * 2 + 1));
+                motor2.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x) / (gamepad2.right_trigger * 2 + 1) / 2 * (gamepad2.left_trigger * 2 + 1));
             } else {
-                armMotor.setPower(gamepad1.left_stick_y);
+                motor1.setPower(gamepad2.left_stick_y + gamepad2.right_stick_x);
+                motor2.setPower(-gamepad2.left_stick_y + gamepad2.right_stick_x);
             }
 
-            if (gamepad1.triangle){
-                ignition.setPosition(1);
-            }
-            else if (gamepad1.x){
-                ignition.setPosition(0.5);
-            }
+            telemetry.addData("Gamepad2? => ",gm2);
             telemetry.update();
         }
 
