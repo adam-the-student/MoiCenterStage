b4085732bd1ee1e75e560cf79964392738c53cc0
Adam Jaffery <Adamjaffery4@gmail.com>
2024-02-22 20:28:05 -0600

    ig everything else is ok - Fawwaz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpikeAndApriltagDetectionAutoRightSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpikeAndApriltagDetectionAutoRightSide.java
@@ -4,22 +4,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.Opencv.CvMaster;
 import org.firstinspires.ftc.teamcode.Opencv.Pipelines.AprilTagDetectionPipeline;
-import org.firstinspires.ftc.teamcode.Opencv.Pipelines.SpikeDetectionThreeZone;
-import org.firstinspires.ftc.teamcode.Opencv.Pipelines.workspace.SpikeZoneDetectionBlue;
 import org.firstinspires.ftc.teamcode.Opencv.Pipelines.workspace.SpikeZoneDetectionRed;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-
-import java.util.ArrayList;
 
 @Autonomous(name = "Spike+AprilTag Detection Right")
 public class SpikeAndApriltagDetectionAutoRightSide extends LinearOpMode {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/PipelineTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/PipelineTesting.java
@@ -4,8 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Opencv.CvMaster;
-import org.firstinspires.ftc.teamcode.Opencv.Pipelines.SpikeDetectionThreeZone;
-import org.firstinspires.ftc.teamcode.Opencv.Pipelines.workspace.SpikeZoneDetectionBlue;
 import org.firstinspires.ftc.teamcode.Opencv.Pipelines.workspace.SpikeZoneDetectionRed;
 
 @Disabled
